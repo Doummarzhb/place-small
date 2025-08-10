c68675c5a281dc66ad242c03948bd383fb360185
Doummar M.alzahabi <98951369+Doummarzhb@users.noreply.github.com>
2025-08-10 18:33:47 +0300

    F

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,5 +1,7 @@
+import 'package:applicationnplace/screens/place_detail.dart';
 import 'package:flutter/material.dart';
 import '../models/place.dart';
+ 
 
 class PlacesList extends StatefulWidget {
   const PlacesList({super.key, required this.places});
@@ -14,11 +16,17 @@ class _PlacesListState extends State<PlacesList> {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: widget.places.length, 
+      itemCount: widget.places.length,
       itemBuilder: (ctx, index) => ListTile(
         title: Text(widget.places[index].title),
-        // style: theme.of(context).textTheme.titleMedium!.copyWith(),
-        
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => PlaceDetailScreen(place: widget.places[index]),
+            ),
+          );
+        },
       ),
     );
   }
